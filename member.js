aef3cf60568ed19b64add237020b58a6a94d974b
Amir Masoud Nourollah <61701369+Nourollah@users.noreply.github.com>
2024-04-16 16:32:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = document.getElementById("member").value;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsArray = memberSkills.value.split(",");
+    var memberSkillsArrayLength = memberSkillsArray.length;
+    var memberSkillsArrayIndex = memberSkillsArray.indexOf(member);
+    if (memberSkillsArrayIndex > -1) {
+        memberSkillsArray.splice(memberSkillsArrayIndex, 1);
+        memberSkills.value = memberSkillsArray.join(",");
+    } else {
+        memberSkillsArray.push(member);
+        memberSkills.value = memberSkillsArray.join(",");
+    }
+}
